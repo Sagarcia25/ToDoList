42f9a7437cbcbeb7b799bf70620daa82b3507620
Salvador <Sagarcia2713@gmail.co>
2019-03-28 19:47:38 -0500

    Deleted ViewController.swift file and the scene in main.storyboard. Did this to be able to implement a new navigation controller with a table view. Changed the title of the table view to "To Do List", and checked the prefer large titles box in order to display the tittle in large font and aligned to the left.

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Salvador Garcia on 3/28/19.
+//  Copyright © 2019 Salvador Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
