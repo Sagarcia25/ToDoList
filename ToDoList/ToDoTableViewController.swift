ec84092701105c51ad4c24dd763db097ef65fa30
Salvador <Sagarcia2713@gmail.co>
2019-03-28 22:09:17 -0500

    A new navigation controller has been added to the storyboard in order to add new todo objects by the user. The right bar button that was added pushes modally to the new todo list table view screen. Uncommented canEditRowAt and editingStyle functions in order to be able to edit and delete todo objects on the tableview.  Added a left edit bar button programmatically in the ViewDidLoad in order for the user to be able to edit each item if they are not familiar with the swipe to delete.

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -14,6 +14,7 @@ class ToDoTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
 
         if let savedToDos = ToDo.loadTodos(){
             toDos = savedToDos
@@ -61,25 +62,25 @@ class ToDoTableViewController: UITableViewController {
     }
  
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            toDos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
