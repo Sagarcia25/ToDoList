0df7f148891dd7f99591738e4477e8b273505303
Salvador <Sagarcia2713@gmail.co>
2019-03-28 20:07:29 -0500

    Created a new ToDo swift file to input my model aspect of the project. Within the file, I created a ToDo struct. Made a ToDoTableViewController in order to connect to the table view in main.storyboard. created a toDos array object with ToDo properties. This was done in order to add items in the array to populate the array later on.

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Salvador Garcia on 3/28/19.
+//  Copyright © 2019 Salvador Garcia. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var duedate: Date
+    var note: String?
+}
